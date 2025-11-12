53307fb8cae070ffaa16f5e48a03f9bf4c972227
Nicolai-Bundgaard-Sorensen <niso21@elev.techcollege.dk>
2025-11-12 10:37:37 +0100

    Mulig Fix på View & Controller

--- assets/js/Controllers/dagensRetController.js
+++ assets/js/Controllers/dagensRetController.js
@@ -2,24 +2,9 @@ import { Layout } from "./layoutController.js"
 import { getDagensRet } from "../models/dagensRetModel.js"
 import { DagensRetView } from "../views/molecules/DagensRetView.js"
 
-// Controller til "Dagens Ret"-siden
 export const DagensRetPage = async () => {
     const title = "Dagens Ret"
-
-    try {
-        //  Hent data fra modellen (API eller lokal data)
-        const dagensRet = await getDagensRet()
-
-        //  Opret view ud fra dataen
-        const content = DagensRetView(dagensRet)
-
-        //  Pak view’et ind i det fælles layout (header, nav, main, footer)
-        return await Layout(title, content)
-    } catch (error) {
-        console.error("Fejl i DagensRetPage:", error)
-
-        // Fejlvisning i tilfælde af at API-kaldet fejler
-        const errorContent = DagensRetView(null)
-        return await Layout(title, errorContent)
-    }
+    const dagensRet = await getDagensRet()
+    const view = DagensRetView(dagensRet)
+    return await Layout(title, view)
 }
--- assets/js/View/molycules/DagensRetView.js
+++ assets/js/View/molycules/DagensRetView.js
@@ -1,14 +1,11 @@
 import { Fragment, Paragraph, Heading } from "../atoms/index.js"
 
-// View-funktion der viser "Dagens Ret"
 export const DagensRetView = (dagensRet) => {
     const element = Fragment()
 
-    // Overskrift
     const h2 = Heading(2)
     h2.innerText = "Dagens Ret"
 
-    // Tekstafsnit
     const p = Paragraph()
 
     if (dagensRet) {
@@ -21,8 +18,6 @@ export const DagensRetView = (dagensRet) => {
         p.innerText = "Der er ingen Dagens Ret lige nu."
     }
 
-    // Tilføj alt i rækkefølge
     element.append(h2, p)
-
     return element
 }
