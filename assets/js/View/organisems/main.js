57efce91c7062489d47575937970efa486bc90ec
Nicolai-Bundgaard-Sorensen <niso21@elev.techcollege.dk>
2025-11-12 13:03:35 +0100

    Connect Views

---
+++ assets/js/View/organisems/main.js
@@ -0,0 +1,10 @@
+import { view1 } from './BusView.js';
+import { view2 } from './DagensRetView.js';
+import { view3 } from './mainView.js';
+import { view4 } from './skemaView.js';
+
+// Run all views
+view1();
+view2();
+view3();
+view4();
